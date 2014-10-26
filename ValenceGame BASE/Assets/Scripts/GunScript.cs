3731361701c8f1cf4b2bb74d62e4d28bf7f75157
jaycaz <jacazamias@gmail.com>
2014-10-25 19:02:13 -0500

    Add Tank struct to hold tank variables

--- ValenceGame BASE/Assets/Scripts/GunScript.cs
+++ ValenceGame BASE/Assets/Scripts/GunScript.cs
@@ -1,260 +1,281 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public struct Tank {
+	// Name of compound stored in tank
+	public string name;
+	// Rate at which a compound will be consumed
+	public int rate;
+	// Current amount of compound stored in tank
+	public int capacity;
+
+	public Tank(string n, int rt, int cp)
+	{
+		name = n;
+		rate = rt;
+		capacity = cp;
+	}
+
+	public Tank(string n, int rt)
+	: this(n, rt, 0)
+	{}
+}
+
 public class GunScript : MonoBehaviour
 {
-    public int power;
-    //never used in GunScript
-    public int numclicks;
-    //what is this for?
+	public int power;
+	//never used in GunScript
+	public int numclicks;
+	//what is this for?
 
-    //public GameObject emitter;  //HARDCODE
+	//public GameObject emitter;  //HARDCODE
 
-    public bool isEmitting;
-    public bool isEmpty;
-    public bool reactSelected;    //was 'isEquation' //is there a specific equation that is being used to get elements
+	public bool isEmitting;
+	public bool isEmpty;
+	public bool reactSelected;	//was 'isEquation' //is there a specific equation that is being used to get elements
+		//what does this do?
+	public bool eqBalanced;		//was 'balanced'
 
-    public bool eqBalanced;		//was 'balanced'
+	public GameObject effect;   //HARDCODE
 
-    public GameObject effect;   //HARDCODE
+	// Tanks for reactants
+	public Tank reactTank1;
+	public Tank reactTank2;
+	public Tank reactTank3;
 
-    //compound capacities in tank
-    public int combineCap;		//was 'capacity'
-    public int tank1Cap;		//was 'elem1capacity'
-    public int tank2Cap;		//was 'elem2capacity'
-    public int tank3Cap;
-    private int fullCap = 400; //needs to be private to be accessed  by gui
+	// Tanks for products
+	public Tank prodTank1;
+	public Tank prodTank2;
+	public Tank prodTank3;
 
-    //compond names and compound rates will need to be set by the reaction
-    //compound names
-    public string cursorName;	//was "elemName"
-    public string tank1Name;	//was 'elem1Name'
-    public string tank2Name;	//was 'elem2Name'
+	//compound capacities in tank
+	public int combineCap;	
+	private int fullCap = 400; //needs to be private to be accessed  by gui
 
-    //compound use rates
-    public int tank1Rate;		//was 'rateElem1'
-    public int tank2Rate;		//was 'rateElem2'
+	//compond names and compound rates will need to be set by the reaction
+	//compound names
+	public string cursorName;	//was "elemName"
 
-    public int sprayDamage;		//to use with different elements and objects
+	public int sprayDamage;		//to use with different elements and objects
 
-    public string chemToShootName;
-    public Chemical.Compound chemToShoot;
+	public string chemToShootName;
+	public Chemical.Compound chemToShoot;
    	public Chemical.Reaction activeReact;
-    public GameObject shootEffect;
+	public GameObject shootEffect;
 
-    //used for different particle emitters
-    public GameObject absorb1;		//for compound 1
-    public GameObject absorb2;		//for compound 2
+	//used for different particle emitters
+	public GameObject absorb1;		//for compound 1
+	public GameObject absorb2;		//for compound 2
 
-    public int getFullCap()  //So GUI can know the maximum capacity in order to scale the gui to screen
-    {
-        return fullCap;
-    }
+	public int getFullCap()  //So GUI can know the maximum capacity in order to scale the gui to screen
+	{
+		return fullCap;
+	}
 
-    // Use this for initialization
-    void Start()
-    {
-        isEmpty = true;
-        isEmitting = false;
-        eqBalanced = false;
-        reactSelected = false;
-        combineCap = 0;
-        cursorName = " ";
-        tank1Rate = 2;		//unless reaction needed, no need to adjust rates
-        tank2Rate = 0;		//can only absorb one element when reaction not selected
-        //perhaps include tankElem3 at some point?
+	// Use this for initialization
+	void Start()
+	{
+		isEmpty = true;
+		isEmitting = false;
+		eqBalanced = false;
+		reactSelected = false;
+		combineCap = 0;
+		cursorName = " ";
+		reactTank1 = new Tank ("O2", 1);  // HARD CODED
+		reactTank2 = new Tank ("H2", 2);  // HARD CODED
 
-        sprayDamage = 0;
-    }
+		//perhaps include tankElem3 at some point?
 
-    // Update is called once per frame
-    void Update()
-    {
-        //venting
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            tank1Cap = 0;
-            tank2Cap = 0;
-        }
-        //identifying elements
-        Ray ray1 = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+		sprayDamage = 0;
+	}
 
-        RaycastHit hit;
-        Physics.Raycast(ray1, out hit, 1000f);
+	// Update is called once per frame
+	void Update()
+	{
+		//venting
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			reactTank1.capacity = 0;
+			reactTank2.capacity = 0;
+		}
+		//identifying elements
+		Ray ray1 = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
-        if (hit.transform.tag == "Absorbable")
-        {
-            if (hit.distance < 8)
-            {
-                cursorName = hit.transform.name;
-            }
-        }
-        else
-        {
-            cursorName = " ";
-        }
+		RaycastHit hit;
+		Physics.Raycast(ray1, out hit, 1000f);
 
-        if (eqBalanced && this.gameObject.GetComponent<Chemical.Compound>() == null)
-        {
-            //SEMI-HARDCODE NEEDS TO BE CHANGED WHEN PRODUCT SELECTION IMPLEMENTED
-            chemToShootName = activeReact.Product1.CompoundName;
+		if (hit.transform.tag == "Absorbable")
+		{
+			if (hit.distance < 8)
+			{
+				cursorName = hit.transform.name;
+			}
+		}
+		else
+		{
+			cursorName = " ";
+		}
 
-            chemToShoot = this.gameObject.AddComponent(chemToShootName) as Chemical.Compound;
-            chemToShoot.init();
+		if (eqBalanced && this.gameObject.GetComponent<Chemical.Compound>() == null)
+		{
+			//SEMI-HARDCODE NEEDS TO BE CHANGED WHEN PRODUCT SELECTION IMPLEMENTED
+			chemToShootName = activeReact.Product1.CompoundName;
 
-            shootEffect = GameObject.Find("ShootGun").GetComponent<GunParticleSwitcher>().setParticleSystem(chemToShoot.state);
+			chemToShoot = this.gameObject.AddComponent(chemToShootName) as Chemical.Compound;
+			chemToShoot.init();
 
-        }
+			shootEffect = GameObject.Find("ShootGun").GetComponent<GunParticleSwitcher>().setParticleSystem(chemToShoot.state);
 
-        //identifying objects (for damaging)
-        if (this.gameObject.GetComponent<Chemical.Compound>() != null && !isEmpty)
-        {
-            sprayDamage = chemToShoot.damage(hit.transform.name);
-            //this will be used in script Extinguished
+		}
 
-            //shootEffect.GetComponent<Extinguished>().particleDamage = sprayDamage;
-        }
+		//identifying objects (for damaging)
+		if (this.gameObject.GetComponent<Chemical.Compound>() != null && !isEmpty)
+		{
+			sprayDamage = chemToShoot.damage(hit.transform.name);
+			//this will be used in script Extinguished
 
-        //absorbing
-        if (Input.GetMouseButton(1) && (eqBalanced || !reactSelected))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+			//shootEffect.GetComponent<Extinguished>().particleDamage = sprayDamage;
+		}
 
-            RaycastHit hit2;
-            Physics.Raycast(ray, out hit2, 1000f);
+		//absorbing
+		if (Input.GetMouseButton(1) && (eqBalanced || !reactSelected))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
-            if (hit2.transform.tag == "Absorbable")
-            {
-                if (hit2.distance < 8)
-                {
-                    Quaternion q = Quaternion.identity;
-                    q.eulerAngles = new Vector3(hit2.normal.x - ray.direction.x, hit2.normal.y - ray.direction.y, hit2.normal.z - ray.direction.z);
+			RaycastHit hit2;
+			Physics.Raycast(ray, out hit2, 1000f);
 
-                    if (reactSelected && activeReact != null)
-                    {	//specific absorb if specific reaction selected
-                        if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == activeReact.Reactant1.getFormula())
-                        {
-                            tank1Name = activeReact.Reactant1.getFormula();
-                            tank1Rate = activeReact.ReactCoeff1;
-                            if (tank1Cap < fullCap)
-                            {
-                                tank1Cap += 2;
+			if (hit2.transform.tag == "Absorbable")
+			{
+				if (hit2.distance < 8)
+				{
+					Quaternion q = Quaternion.identity;
+					q.eulerAngles = new Vector3(hit2.normal.x - ray.direction.x, hit2.normal.y - ray.direction.y, hit2.normal.z - ray.direction.z);
 
-                                Instantiate(effect, hit2.point, q);
-                                isEmpty = false;
+					if (reactSelected && activeReact != null)
+					{	//specific absorb if specific reaction selected
+						if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == activeReact.Reactant1.getFormula())
+						{
+							reactTank1.name = activeReact.Reactant1.getFormula();
+							reactTank1.rate = activeReact.ReactCoeff1;
+							if (reactTank1.capacity < fullCap)
+							{
+								reactTank1.capacity += 2;
 
-                            }
-                        }
-                        if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == activeReact.Reactant2.getFormula())
-                        {
-                            tank2Name = activeReact.Reactant2.getFormula();
-                            tank2Rate = activeReact.ReactCoeff2;
+								Instantiate(effect, hit2.point, q);
+								isEmpty = false;
 
-                            if (tank2Cap < fullCap)
-                            {
-                                tank2Cap += 2;
+							}
+						}
+						if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == activeReact.Reactant2.getFormula())
+						{
+							reactTank2.name = activeReact.Reactant2.getFormula();
+							reactTank2.rate = activeReact.ReactCoeff2;
 
-                                Instantiate(effect, hit2.point, q);
-                                isEmpty = false;
-                            }
-                        }
-                    }
-                    else if (!reactSelected)
-                    {	//to absorb anything without specific reaction selected
-                        if (tank1Cap == 0)  //can absorb anything into tank1
-                        {
-                            tank1Name = hit2.transform.GetComponent<Chemical.Compound>().getFormula();
-                            chemToShoot = this.gameObject.AddComponent(tank1Name) as Chemical.Compound;
-                            chemToShoot.init();
+							if (reactTank2.capacity < fullCap)
+							{
+								reactTank2.capacity += 2;
+								Instantiate(effect, hit2.point, q);
+								isEmpty = false;
+							}
+						}
+					}
+					else if (!reactSelected)
+					{	//to absorb anything without specific reaction selected
+						if (reactTank1.capacity == 0)  //can absorb anything into tank1
+						{
+							reactTank1.name = hit2.transform.GetComponent<Chemical.Compound>().getFormula();
+							chemToShoot = this.gameObject.AddComponent(reactTank1.name) as Chemical.Compound;
+							chemToShoot.init();
 
-                            shootEffect = GameObject.Find("ShootGun").GetComponent<GunParticleSwitcher>().setParticleSystem(chemToShoot.state);
-                            
-                            tank1Cap += 2;
+							shootEffect = GameObject.Find("ShootGun").GetComponent<GunParticleSwitcher>().setParticleSystem(chemToShoot.state);
+							
+							reactTank1.capacity += 2;
 
-                            Instantiate(effect, hit2.point, q);
-                            isEmpty = false;
-                        }
-                        else if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == tank1Name)
-                        {
-                            if (tank1Cap < fullCap)
-                            {
-                                tank1Cap += 2;
+							Instantiate(effect, hit2.point, q);
+							isEmpty = false;
+						}
+						else if (hit2.transform.GetComponent<Chemical.Compound>().getFormula() == reactTank1.name)
+						{
+							if (reactTank1.capacity < fullCap)
+							{
+								reactTank1.capacity += 2;
 
-                                Instantiate(effect, hit2.point, q);
-                                isEmpty = false;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+								Instantiate(effect, hit2.point, q);
+								isEmpty = false;
+							}
+						}
+					}
+				}
+			}
+		}
 
 
-        //shooting
-        //if (eqBalanced)
-        if (eqBalanced || !reactSelected)	//to allow to shoot if no reaction selected
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                numclicks++;
-                if (!isEmitting && !isEmpty)	//will need to code the absorb mechanic
-                {
-                    isEmitting = true;
-                    shootEffect.particleSystem.Play();
-                }
-            }
-            if (Input.GetButton("Fire1"))
-            {
-                if (isEmitting && !isEmpty)
-                {
-                    if (reactSelected && activeReact != null) //is using more than one element
-                    {
-                        if (tank1Cap > 0 && tank2Cap > 0)
-                        {
-                            isEmpty = false;
-                            isEmitting = true;
-                            shootEffect.particleSystem.Play();
-                            tank1Cap -= 1 * tank1Rate;
-                            tank2Cap -= 1 * tank2Rate;
-                        }
-                        else
-                        {
-                            isEmpty = true;
-                            isEmitting = false;
-                            shootEffect.particleSystem.Stop();
-                        }
-                    }
-                    else //not a reaction, just single compound
-                    {
-                        if (tank1Cap > 0)
-                        {
-                            isEmpty = false;
-                            isEmitting = true;
-                            shootEffect.particleSystem.Play();
-                            tank1Cap -= 1 * tank1Rate;
-                        }
-                        else
-                        {
-                            isEmpty = true;
-                            isEmitting = false;
-                            shootEffect.particleSystem.Stop();
-                        }
-                    }
-                }
-            }
-            if (Input.GetButtonUp("Fire1")) //stop emitting
-            {
-                if (isEmitting)
-                {
-                    isEmitting = false;
-                    shootEffect.particleSystem.Stop();
+		//shooting
+		//if (eqBalanced)
+		if (eqBalanced || !reactSelected)	//to allow to shoot if no reaction selected
+		{
+			if (Input.GetButtonDown("Fire1"))
+			{
+				numclicks++;
+				if (!isEmitting && !isEmpty)	//will need to code the absorb mechanic
+				{
+					isEmitting = true;
+					shootEffect.particleSystem.Play();
+				}
+			}
+			if (Input.GetButton("Fire1"))
+			{
+				if (isEmitting && !isEmpty)
+				{
+					if (reactSelected && activeReact != null) //is using more than one element
+					{
+						if (reactTank1.capacity > 0 && reactTank2.capacity > 0)
+						{
+							isEmpty = false;
+							isEmitting = true;
+							shootEffect.particleSystem.Play();
+							reactTank1.capacity -= 1 * reactTank1.rate;
+							reactTank2.capacity -= 1 * reactTank2.rate;
+						}
+						else
+						{
+							isEmpty = true;
+							isEmitting = false;
+							shootEffect.particleSystem.Stop();
+						}
+					}
+					else //not a reaction, just single compound
+					{
+						if (reactTank1.capacity > 0)
+						{
+							isEmpty = false;
+							isEmitting = true;
+							shootEffect.particleSystem.Play();
+							reactTank1.capacity -= 1 * reactTank1.rate;
+						}
+						else
+						{
+							isEmpty = true;
+							isEmitting = false;
+							shootEffect.particleSystem.Stop();
+						}
+					}
+				}
+			}
+			if (Input.GetButtonUp("Fire1")) //stop emitting
+			{
+				if (isEmitting)
+				{
+					isEmitting = false;
+					shootEffect.particleSystem.Stop();
 
-                    if (tank1Cap <= 0 && tank1Cap <= 0)
-                    {
-                        isEmpty = true;
-                    }
-                }
-            }
-        }
-    }
+					if (reactTank1.capacity <= 0 && reactTank1.capacity <= 0)
+					{
+						isEmpty = true;
+					}
+				}
+			}
+		}
+	}
 }
--- ValenceGame BASE/Assets/Scripts/myGUI.cs
+++ ValenceGame BASE/Assets/Scripts/myGUI.cs
@@ -27,64 +27,65 @@ public class myGUI : MonoBehaviour
 	
 	private GUIStyle guiStyle;
 	
-    public string productName;
+	public string productName;
 
 	void Start()
 	{
 		cursorPic = (Texture2D)Resources.Load("cursor");
 		guiStyle = new GUIStyle();
 		guiStyle.richText = true;
-		guiStyle.fontSize = 24;     //how to change wrt width/height?
+		guiStyle.fontSize = 24;	 //how to change wrt width/height?
 		guiStyle.normal.textColor = Color.white;
 		
 		fullCap = player.GetComponent<GunScript>().getFullCap();
 	
-	    productName = "";
+		productName = "";
 	}
 	
 	void OnGUI()
 	{
 		/*scale.y = (float)Screen.height / (float)relScreenH;
-        scale.x = scale.y; 
-        scale.z = 1;
+		scale.x = scale.y; 
+		scale.z = 1;
 
-        float scaleX = (float)Screen.width / (float)relScreenW;
-        Matrix4x4 m = GUI.matrix;
+		float scaleX = (float)Screen.width / (float)relScreenW;
+		Matrix4x4 m = GUI.matrix;
 
-        GUI.matrix = Matrix4x4.TRS(new Vector3((scaleX-scale.y)/2 * relScreenW, 0, 0), Quaternion.identity, scale);
+		GUI.matrix = Matrix4x4.TRS(new Vector3((scaleX-scale.y)/2 * relScreenW, 0, 0), Quaternion.identity, scale);
 
-        length1 = (float)player.GetComponent<GunScript>().tank1Cap * (((float)Screen.height - 100) / (float)fullCap); //is relative to screen size
-        length2 = (float)player.GetComponent<GunScript>().tank2Cap * (((float)Screen.height - 100) / (float)fullCap);
+		length1 = (float)player.GetComponent<GunScript>().tank1Cap * (((float)Screen.height - 100) / (float)fullCap); //is relative to screen size
+		length2 = (float)player.GetComponent<GunScript>().tank2Cap * (((float)Screen.height - 100) / (float)fullCap);
 
-        GUI.skin = mySkin;
+		GUI.skin = mySkin;
 
-        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100), cursorPic);
-        if (player.GetComponent<GunScript>().eqBalanced)
-        //if (player.GetComponent<GunScript>().balanced || !player.GetComponent<GunScript>().isEquation)
-        {
-            GUI.Box(new Rect(10, (Screen.height - 10) - length1, 30, length1), "", "redbar");
-            GUI.Box(new Rect(50, (Screen.height - (30 * player.GetComponent<GunScript>().tank1Rate)), 30, (30 * player.GetComponent<GunScript>().tank1Rate) - 10), "", "whitebar");
-            GUI.Box(new Rect(110, (Screen.height - 10) - length2, 30, length2), "", "bluebar");
-            GUI.Box(new Rect(160, (Screen.height - (30 * player.GetComponent<GunScript>().tank2Rate)), 30, (30 * player.GetComponent<GunScript>().tank2Rate) - 10), "", "whitebar");
+		GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100), cursorPic);
+		if (player.GetComponent<GunScript>().eqBalanced)
+		//if (player.GetComponent<GunScript>().balanced || !player.GetComponent<GunScript>().isEquation)
+		{
+			GUI.Box(new Rect(10, (Screen.height - 10) - length1, 30, length1), "", "redbar");
+			GUI.Box(new Rect(50, (Screen.height - (30 * player.GetComponent<GunScript>().tank1Rate)), 30, (30 * player.GetComponent<GunScript>().tank1Rate) - 10), "", "whitebar");
+			GUI.Box(new Rect(110, (Screen.height - 10) - length2, 30, length2), "", "bluebar");
+			GUI.Box(new Rect(160, (Screen.height - (30 * player.GetComponent<GunScript>().tank2Rate)), 30, (30 * player.GetComponent<GunScript>().tank2Rate) - 10), "", "whitebar");
 
-            GUI.Box(new Rect(10, 10, 50, 50), "   H2O", "whitebar");
-        }
+			GUI.Box(new Rect(10, 10, 50, 50), "   H2O", "whitebar");
+		}
 
-        GUI.Label(new Rect(Screen.width / 2 - 20, Screen.height / 2 - 60, 50, 50), player.GetComponent<GunScript>().cursorName, guiStyle);
+		GUI.Label(new Rect(Screen.width / 2 - 20, Screen.height / 2 - 60, 50, 50), player.GetComponent<GunScript>().cursorName, guiStyle);
 
-        GUI.matrix = m;
-        */
+		GUI.matrix = m;
+		*/
 		ratH = (float)Screen.height / (float)relScreenH;
 		ratW = (float)Screen.width / (float)relScreenW;
 		
-		length1 = (float)player.GetComponent<GunScript>().tank1Cap * (((float)Screen.height - (100 * (float)ratH)) / (float)fullCap); //is relative to screen size
-		length2 = (float)player.GetComponent<GunScript>().tank2Cap * (((float)Screen.height - (100 * (float)ratH)) / (float)fullCap);
-		string tank1Name = player.GetComponent<GunScript> ().tank1Name;
+		length1 = (float)player.GetComponent<GunScript>().reactTank1.capacity * (((float)Screen.height - (100 * (float)ratH)) / (float)fullCap); //is relative to screen size
+		length2 = (float)player.GetComponent<GunScript>().reactTank2.capacity * (((float)Screen.height - (100 * (float)ratH)) / (float)fullCap);
+		string tank1Name = player.GetComponent<GunScript>().reactTank1.name;
+		string tank2Name = player.GetComponent<GunScript>().reactTank2.name;
 		GUI.skin = mySkin;
 		
 		GUI.Label(new Rect(Screen.width / 2 - (50 * ratW), Screen.height / 2 - (50 * ratH), 100 * ratW, 100 * ratH), cursorPic);
 	
-        //Do need to show GUI all the time
+		//Do need to show GUI all the time
 		//if (player.GetComponent<GunScript>().eqBalanced)
 			//if (player.GetComponent<GunScript>().balanced || !player.GetComponent<GunScript>().isEquation)
 		//{
@@ -127,9 +128,9 @@ public class myGUI : MonoBehaviour
 			
 			GUI.color = Color.black;
 			//Element/Compound names
-			GUI.Button(new Rect(10 * ratW, (Screen.height - 30*ratH), 20 * ratW, 20*ratH), player.GetComponent<GunScript> ().tank1Name);
+			GUI.Button(new Rect(10 * ratW, (Screen.height - 30*ratH), 20 * ratW, 20*ratH), tank1Name);
 			//GUI.Button(new Rect(50 * ratW, (Screen.height - 30*ratH), 30 * ratW, 20*ratH), player.GetComponent<GunScript> ().tank1Name);
-			GUI.Button(new Rect(40 * ratW, (Screen.height - 30*ratH), 20 * ratW, 20*ratH), player.GetComponent<GunScript> ().tank2Name);
+			GUI.Button(new Rect(40 * ratW, (Screen.height - 30*ratH), 20 * ratW, 20*ratH), tank2Name);
 			
 			//GUI.Box(new Rect(10 * ratW, 10 * ratH, 50 * ratW, 50 * ratH), "   H2O", "whitebar");
 			//GUI.Button(new Rect(10 * ratW, (Screen.height - (10 * ratH)) - 180, 50 * ratW, 50 * ratH), "   H2O");
