ca170196a51925a1885268cc43180caa300463fd
jedioli <jedi_oli@tamu.edu>
2014-10-29 14:09:50 -0500

    working on ice melting

---
+++ ValenceGame BASE/Assets/Scripts/Chemicals/Compounds/IceWater.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IceWater : Chemical.Compound {
+	
+	
+	//	public Water() : base("H2O") {
+	//
+	//		atoms = new Dictionary<Chemical.Element, int>();
+	//		atoms.Add (new Hydrogen(), 2);
+	//		atoms.Add (new Oxygen (), 1);
+	//	}
+	
+	
+	
+	public override int damage (string obstacleName) {
+		
+		if(obstacleName == "Firewall") {
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	public override int heal (string obstacleName) {
+		
+		if(obstacleName == "Firewall") {
+			return 0;
+		}
+		
+		return 0;
+	}
+	
+	
+	/*
+	public override void interact() {
+		/*
+		use "is()" to check type!
+		 *
+	}
+
+	public override bool augment() {
+		/*
+		use "is()" to check type!
+		 *
+		return false;
+	}
+
+	public override bool remove() {
+		/*
+		use "is()" to check type!
+		 *
+		return false;
+	}
+
+	*/
+	
+	// Use this for initialization
+	public override void Start () {
+		compoundName = "IceWater";
+		formula = "H2O";
+		
+		atoms = new Dictionary<Chemical.Element, int>();
+		atoms.Add (new Hydrogen(), 2);
+		atoms.Add (new Oxygen (), 1);
+		
+		
+	}
+
+	void OnParticleCollision(GameObject other){
+		if(other.tag == "WaterGunEmitter") {
+			//			Vector3 shrink = new Vector3(0.9, 0.9, 0.9);
+			//			Vector3 aPosition = new Vector3(1, 1, 1);
+			this.gameObject.transform.localScale = Vector3.Scale(this.gameObject.transform.localScale, new Vector3(0.95F, 0.95F, 0.95F));
+			
+			if(this.gameObject.transform.localScale.x < 0.25F) {
+				this.gameObject.gameObject.SetActive(false);
+			}
+			//			Vector3 scale = other.transform.localScale;
+			//			other.transform.localScale.x = scale.x - 0.1F;
+			//			other.transform.localScale.y = scale.y - 0.1F;
+			//			other.transform.localScale.z = scale.z - 0.1F;
+		}
+		//Instantiate(effect, , q);
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		//	
+	}
+}
--- ValenceGame BASE/Assets/Scripts/Extinguished.cs
+++ ValenceGame BASE/Assets/Scripts/Extinguished.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class Extinguished : MonoBehaviour {
+	//CHANGE NAME OF THIS!
+
     public ParticleSystem effect;
 
 	public int particleDamage;
@@ -21,6 +23,20 @@ public class Extinguished : MonoBehaviour {
 
             //Destroy(other);
 		}
+
+//		if(other.name == "IceCube") {
+//			Vector3 shrink = new Vector3(0.9, 0.9, 0.9);
+//			Vector3 aPosition = new Vector3(1, 1, 1);
+//			other.transform.localScale = Vector3.Scale(other.transform.localScale, new Vector3(0.95F, 0.95F, 0.95F));
+
+//			if(other.transform.localScale.x < 0.2F) {
+//				other.gameObject.SetActive(false);
+//			}
+//			Vector3 scale = other.transform.localScale;
+//			other.transform.localScale.x = scale.x - 0.1F;
+//			other.transform.localScale.y = scale.y - 0.1F;
+//			other.transform.localScale.z = scale.z - 0.1F;
+//		}
         //Instantiate(effect, , q);
 	}
 }
