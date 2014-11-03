7f23aad41161e0778f99bfb3a787c1f84ced146e
zlewis <jedimasterzl@yahoo.com>
2014-11-03 13:50:56 -0600

    Tabs to Spaces

--- ValenceGame BASE/Assets/Scripts/iceCollision.cs
+++ ValenceGame BASE/Assets/Scripts/iceCollision.cs
@@ -1,49 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class iceCollision : MonoBehaviour {
+public class iceCollision : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+    
+    }
 
-	void OnCollisionEnter(Collision col) {
+    void OnCollisionEnter (Collision col)
+    {
 
-		// Get the scales and the object that activates them
-		// This script should be applied to both scales, this assumes they are paired off
-		GameObject scale1 = GameObject.Find ("Scale1");
-		GameObject scale2 = GameObject.Find ("Scale2");
-		GameObject box = GameObject.Find ("Box");
-		GameObject partner=null;
+        // Get the scales and the object that activates them
+        // This script should be applied to both scales, this assumes they are paired off
+        GameObject scale1 = GameObject.Find ("Scale1");
+        GameObject scale2 = GameObject.Find ("Scale2");
+        GameObject box = GameObject.Find ("Box");
+        GameObject partner = null;
 
-		if(this.gameObject==scale1){partner=scale2;}
-		else if(this.gameObject==scale2){partner=scale1;}
+        if (this.gameObject == scale1) {
+            partner = scale2;
+        } else if (this.gameObject == scale2) {
+            partner = scale1;
+        }
 
-		Vector3 newPos1 = this.gameObject.transform.position;
-		Vector3 newPos2 = partner.transform.position;
+        Vector3 newPos1 = this.gameObject.transform.position;
+        Vector3 newPos2 = partner.transform.position;
 
-		if (col.gameObject == box) {
-			// Rate at which the platforms move
-			// The original box is ruled by gravity so that effects the rate also
-			// 0.02f and below are too little to work well
-			newPos1.y-=0.05f;
-			newPos2.y+=0.05f;
-			if(newPos1.y<=1) {
-				newPos1.y=1;
-				newPos2.y=partner.transform.position.y;
-			}
-			if(newPos2.y<=1) {
-				newPos2.y=1;
-				newPos1.y=partner.transform.position.y;
-			}
-			this.gameObject.transform.position=newPos1;
-			partner.transform.position=newPos2;
-		}
-	}
+        if (col.gameObject == box) {
+            // Rate at which the platforms move
+            // The original box is ruled by gravity so that effects the rate also
+            // Rates below 0.1 can cause problems where the platform only moves once
+            newPos1.y -= 0.1f;
+            newPos2.y += 0.1f;
+            if (newPos1.y <= 1) {
+                newPos1.y = 1;
+                newPos2.y = partner.transform.position.y;
+            }
+            if (newPos2.y <= 1) {
+                newPos2.y = 1;
+                newPos1.y = partner.transform.position.y;
+            }
+            this.gameObject.transform.position = newPos1;
+            partner.transform.position = newPos2;
+        }
+    }
 }
--- ValenceGame BASE/Assets/Scripts/pickUpObject.cs
+++ ValenceGame BASE/Assets/Scripts/pickUpObject.cs
@@ -4,82 +4,72 @@ using System.Collections;
 public class pickUpObject : MonoBehaviour
 {
 
-	public bool isNear=false;
-	public bool pickUp=false;
+    public bool isNear = false;
+    public bool pickUp = false;
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		// isNear
-		float distance = 2.0f;
-		GameObject player = GameObject.Find ("Player");
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        // isNear
+        float distance = 2.0f;
+        GameObject player = GameObject.Find ("Player");
 
-		GameObject box = GameObject.Find ("Box"); // Object you want to move!!! You should be able to change the string and it should work with whatever
+        GameObject box = GameObject.Find ("Box"); // Object you want to move!!! You should be able to change the string and it should work with whatever
 
-		GameObject cam = GameObject.Find ("Main Camera");
+        GameObject cam = GameObject.Find ("Main Camera");
 
-		if ((player.transform.position - box.transform.position).magnitude < distance)
-		{
-			isNear = true;
-		} 
-		else
-		{
-			isNear = false;
-		}
+        if ((player.transform.position - box.transform.position).magnitude < distance) {
+            isNear = true;
+        } else {
+            isNear = false;
+        }
 
-		// Hitting E and picking up and dropping
-		if (Input.GetKeyDown(KeyCode.E))
-		{   
-			if(pickUp)
-			{
-				pickUp=false;
-				box.rigidbody.useGravity=true;
-			}
-			else if (isNear)
-			{
-				pickUp = true;
-			}
-		}
+        // Hitting E and picking up and dropping
+        if (Input.GetKeyDown (KeyCode.E)) {   
+            if (pickUp) {
+                pickUp = false;
+                box.rigidbody.useGravity = true;
+            } else if (isNear) {
+                pickUp = true;
+            }
+        }
 
-		// After pick up
-		Vector3 newPos;
-		newPos.x = player.transform.position.x + (player.transform.forward.x * 5);
-		newPos.z = player.transform.position.z + (player.transform.forward.z * 5);
-		newPos.y = player.transform.position.y + (cam.transform.forward.y * 8);
+        // After pick up
+        Vector3 newPos;
+        newPos.x = player.transform.position.x + (player.transform.forward.x * 5);
+        newPos.z = player.transform.position.z + (player.transform.forward.z * 5);
+        newPos.y = player.transform.position.y + (cam.transform.forward.y * 8);
 
-		if(newPos.y<=1)
-		{
-			newPos.y=1;
-		}
+        if (newPos.y <= 1) {
+            newPos.y = 1;
+        }
 
-		if (pickUp)
-		{
-			box.transform.position=newPos;
-			box.rigidbody.useGravity=false;
-		}
+        if (pickUp) {
+            box.transform.position = newPos;
+            box.rigidbody.useGravity = false;
+        }
 
-	}
+    }
 
-	void OnGUI() 
-	{
-		GameObject player = GameObject.Find ("Player");
-		GameObject box = GameObject.Find ("Box");
+    void OnGUI ()
+    {
+        GameObject player = GameObject.Find ("Player");
+        GameObject box = GameObject.Find ("Box");
 
-		// Trigger prompt text when close to object
-		if (isNear)
-		{
-			showText();
-		}
-	}
+        // Trigger prompt text when close to object
+        if (isNear) {
+            showText ();
+        }
+    }
 
-	void showText() 
-	{
-		GUI.Label(new Rect(Screen.width/2 - 60, 80, Screen.width, Screen.height), "<size=24>Press 'E' to pick up.</size>");
-	}
+    void showText ()
+    {
+        GUI.Label (new Rect (Screen.width / 2 - 60, 80, Screen.width, Screen.height), "<size=24>Press 'E' to pick up.</size>");
+    }
 }
