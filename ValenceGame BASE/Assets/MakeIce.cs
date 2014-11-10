bada407b4099d68048fd82662c2223fbec5f78d7
Brooke Griffith <griffith.brooke@sbcglobal.net>
2014-11-10 14:36:28 -0600

    Issues

---
+++ ValenceGame BASE/Assets/MakeIce.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeIce : MonoBehaviour {
+	public GameObject player;
+	public GameObject ice;
+	public GameObject target;
+
+	public int capacity;
+	public int maxCapacity = 400;
+	// Use this for initialization
+	void Start () {
+		capacity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnParticleCollision(GameObject other){
+		if (player.GetComponent<GunScript> ().chemToShootName == "H2O") {
+			if(capacity < maxCapacity){	
+				capacity += 1;
+				Instantiate(ice, new Vector3(target.transform.position.x, target.transform.position.y + 2, target.transform.position.z), Quaternion.identity);
+			}
+		}
+	}
+}
