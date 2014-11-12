cbaa7f439d8a838407024ed75671e056780f26d3
Brooke Griffith <griffith.brooke@sbcglobal.net>
2014-11-12 12:18:59 -0600

    Ice machine works

--- ValenceGame BASE/Assets/MakeIce.cs
+++ ValenceGame BASE/Assets/MakeIce.cs
@@ -7,7 +7,7 @@ public class MakeIce : MonoBehaviour {
 	public GameObject target;
 
 	public int capacity;
-	public int maxCapacity = 400;
+
 	// Use this for initialization
 	void Start () {
 		capacity = 0;
@@ -22,7 +22,10 @@ public class MakeIce : MonoBehaviour {
 		if (player.GetComponent<GunScript> ().chemToShootName == "H2O") {
 			if(capacity < maxCapacity){	
 				capacity += 1;
+				//Instantiate(ice, new Vector3(target.transform.position.x, target.transform.position.y + 2, target.transform.position.z), Quaternion.identity);
+			}else if(capacity >= maxCapacity){
 				Instantiate(ice, new Vector3(target.transform.position.x, target.transform.position.y + 2, target.transform.position.z), Quaternion.identity);
+				capacity = 0;
 			}
 		}
 	}
